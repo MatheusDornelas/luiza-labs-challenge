e8186f558ad3977433abf354ab816754f586e18a
Matheus Dornelas <matheusdornelas3@gmail.com>
2024-11-24 13:17:02 -0300

    chore: removing scaffold comments + unnecessary code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,26 +4,18 @@ class UsersController < ApplicationController
   include Pagination
   before_action :set_user, only: %i[show]
 
-  # GET /users
   def index
     users = UsersService.new({ per_page: per_page, page: paginate_offset, order_id: params[:order_id] }).fetch_users
     render json: users, include: [orders: [:products]]
   end
 
-  # GET /users/1
   def show
     render json: @user, include: [orders: [:products]]
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.includes(orders: [:products]).find_by({ external_id: params.expect(:id) })
-  end
-
-  # Only allow a list of trusted parameters through.
-  def user_params
-    params.expect
   end
 end
